5daa4250c3e5ac0f4b61cc6c25de1e3a119c0387
constanza evaristo <connievaristo@gmail.com>
2024-05-04 13:36:22 -0300

    ejercicios hechos

---
+++ js/ejercicios.js
@@ -0,0 +1,20 @@
+//Ejercicios CONSOLA (console.log y console.error)
+console.log("Hola mundo");
+
+let myName = "Constanza";
+console.log(myName);
+
+console.error("error?");
+
+//Ejercicios PROMPT
+let yourName = prompt("Ingresá tu nombre");
+console.log(yourName);
+let yourAge = prompt("Ingresá tu edad");
+console.log("Tu nombre es " + yourName + " y tenés " + yourAge + " años.");
+
+//Ejercicios ALERT
+alert("Bienvenido a Ejercicios Clase 1");
+
+let favFood = prompt("¿Cuál es tu comida favorita?");
+alert("Tu comida favorita es " + favFood);
+
