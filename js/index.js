6794fd31b241b209b019ab6674ea48cc4c4d3b4a
constanza evaristo <connievaristo@gmail.com>
2024-06-05 12:04:43 -0300

    clase10

---
+++ js/index.js
@@ -0,0 +1,77 @@
+//Las variables que ya están declaradas en navegación, las reconoce (tiene que estar antes en el HTML porque es en casacada)
+
+const mainBody = document.querySelector('#main'); //agarré el id main, como es el getElementbyId, como es querySelector va con #
+const boton = document.createElement('button');
+boton.textContent = 'Click'; //que diga click
+boton.type = 'button';
+mainBody.appendChild(boton);
+boton.style.backgroundColor='yellow'; //Estilos con DOM
+boton.style.padding='1rem'; 
+boton.style.width='6rem';
+boton.style.margin='2rem';
+boton.style.fontSize='1.2rem';
+//Hasta acá: DOM
+
+//Eventos
+
+//1. .addEventListener('evento que quiero generar/capturar',funcionAsignada
+//Quiero que me cambie el color cuando aprieto el botón
+
+// function respuestaClick(){ 
+//     boton.style.backgroundColor = 'blue';
+//     boton.style.color = 'white';
+// }
+
+//.addEventListener con función flecha declarada dentro
+// boton.addEventListener('click', () => {
+//     // boton.style.backgroundColor='red';
+//     // boton.style.color='white';
+// })
+
+//2. onclick quiere que le pases una función
+boton.onclick = () => {
+    alert("Botón apretado con onclick!")
+};
+
+//3. Sintaxis en el HTML (buscá el input)
+
+//Más ejemplos:
+boton.onclick = () => {
+    cuerpo.classList.add('black'); //Mira en la consola el HTML para ver si aparece la clase
+};
+
+boton.addEventListener('mouseover',() => {
+    boton.style.color = 'blue';
+    boton.style.backgroundColor = 'yellow'
+});
+
+boton.addEventListener('mouseout', () => {
+    boton.style.color = 'yellow';
+    boton.style.backgroundColor = 'blue';
+});
+
+//Evento change
+let input1 = document.getElementById('nombre');
+let input2 = document.getElementById('edad');
+
+input1.onchange = () => {console.log(input1.value)}; //Traigo el valor ingresado en el input
+input2.onchange = () => {console.log(input2.value)};
+
+//Elemento input
+input1.addEventListener('input', () => {
+    console.log(input1.value)
+});
+
+//Evento submit en contact.js
+
+//Eventos del mouse
+let inicioH1 = document.getElementById('inicioH1');
+console.log(inicioH1.innerHTML); //Inicio
+
+inicioH1.addEventListener('mouseover',() =>{ //se pone azul cuando te parás y hacés click
+    inicioH1.style.color = 'blue';
+});
+
+inicioH1.addEventListener('mouseout', () => { //vuelve a negro cuando salís y hacés click
+    inicioH1.style.color = 'black';
+});
---
+++ js/navegacion.js
@@ -0,0 +1,40 @@
+// Delaración de Variables: buena práctica declararlo así
+const cuerpo = document.body;
+const cabecera = document.getElementById('header');
+const navegacion = document.createElement('div'); 
+const nav = document.createElement('nav');
+const ul = document.createElement('ul');
+const links = ["Index", "Products", "Contact"];
+const liImagen = document.createElement('li');
+const img = document.createElement('img');
+const ORIGEN = document.createElement('a');
+const footer = document.getElementsByTagName('footer');
+const parrafoFooter = document.createElement('p');
+const TIME = new Date().getFullYear(); // 2024
+
+
+cabecera.appendChild(navegacion);
+navegacion.appendChild(nav);
+nav.appendChild(ul);
+navegacion.className = 'navbar'; //Agregar serie de clases: classList add y ponés con ,
+
+
+ORIGEN.href = '/';
+ORIGEN.appendChild(img);
+img.src = 'img/logo.png';
+img.alt = 'CoderHouse';
+
+liImagen.appendChild(ORIGEN);
+ul.appendChild(liImagen);
+
+for (const link of links) {
+    const li = document.createElement('li');
+    li.innerHTML = `<a href="${link.toLowerCase()}.html" >${link}</a>`;
+    ul.appendChild(li);
+}
+
+cabecera.style.backgroundColor = '#c2c2c2';
+
+
+footer[0].appendChild(parrafoFooter);
+parrafoFooter.innerHTML = 'CoderHouse - ' + TIME + ' | Comisión #57705';
