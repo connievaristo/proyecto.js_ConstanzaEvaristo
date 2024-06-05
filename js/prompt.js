1172acc668d83911cf083fe13b5e96ad7904918d
constanza evaristo <connievaristo@gmail.com>
2024-06-05 10:01:41 -0300

    clase10 cambio archivos

--- js/prompt.js
+++
@@ -1,6 +0,0 @@
-let primerNombre = prompt("Ingrese su primer nombre"); //me deja declararlo porque en el HTML solo está el prompt.js conectado
-// console.log(firstName);
-
-const NUMERO = prompt("Ingrese su edad");
-alert("Su edad es de " + NUMERO + " años.");
-console.log("La edad de " + primerNombre + " es de " + NUMERO + " años.");
